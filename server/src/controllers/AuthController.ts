02c0e0a77f91a8d4fcbc916bbc357b0784419612
pankaj kumar <pankajamsali@gmail.com>
2025-03-25 16:44:29 +0530

    implement JWT authentication; add AuthController and routes for user login

---
+++ server/src/controllers/AuthController.ts
@@ -0,0 +1,51 @@
+import { Prisma } from '@prisma/client';
+import { Request, Response } from 'express';
+import prisma from '../config/db.config.js';
+import jwt from 'jsonwebtoken';
+
+interface LoginPayloadType {
+  name: string;
+  email: string;
+  provider: string;
+  oauth_id: string;
+  image?: string;
+}
+class AuthController {
+  static async login(req: Request, res: Response) {
+    try {
+      const body: LoginPayloadType = req.body;
+      let findUser = await prisma.user.findUnique({
+        where: {
+          email: body.email,
+        },
+      });
+      if (!findUser) {
+        findUser = await prisma.user.create({
+          data: body,
+        });
+      }
+      let JWTPayload = {
+        name: body.name,
+        email: body.email,
+        id: findUser.id,
+      };
+
+      const token = jwt.sign(JWTPayload, process.env.JWT_SECRET, {
+        expiresIn: '365d',
+      });
+      return res.json({
+        message: 'User logged in successfully',
+        user: {
+          findUser,
+          token: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Error logging in user',
+      });
+    }
+  }
+}
+
+export default AuthController;
