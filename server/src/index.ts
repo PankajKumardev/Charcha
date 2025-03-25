06800238c62d89fd16c07092a247a6e86506bca0
pankaj kumar <pankajamsali@gmail.com>
2025-03-25 16:09:47 +0530

    initialize Prisma with PostgreSQL setup; add User model and migrations

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
-import express, { Application, Request, Response } from "express";
-import "dotenv/config";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import 'dotenv/config';
+import cors from 'cors';
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
 
@@ -9,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req: Request, res: Response) => {
-  return res.send("It's working 🙌"); 
+app.get('/', (req: Request, res: Response) => {
+  return res.send("It's working 🙌");
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
