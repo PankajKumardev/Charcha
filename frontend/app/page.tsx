dcfe37e2386b9ae6a57d44d57dd4f60087dedab7
pankaj kumar <pankajamsali@gmail.com>
2025-03-25 15:18:52 +0530

    refactor homepage layout and add new components for improved user experience

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,35 +1,27 @@
-import { Button } from '@/components/ui/button';
-
-export default function Home() {
+import Navbar from '@/components/base/Navbar';
+import HeroSection from '@/components/base/HeroSection';
+import FeatureSection from '@/components/base/FeatureSection';
+import UserReviews from '@/components/base/UserReviews';
+import Footer from '@/components/base/Footer';
+// import { authOptions, CustomSession } from './api/auth/[...nextauth]/options';
+// import { getServerSession } from 'next-auth';
+export default async function LandingPage() {
+  // const session: CustomSession | null = await getServerSession(authOptions);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-6xl font-semibold">Charcha</h1>
-      <p className="text-2xl text-gray-500">
-        A platform for meaningful conversations
-      </p>
-      <div className="flex flex-row space-x-4 mt-4">
-        <Button>Get Started</Button>
-        <Button variant="secondary">Learn More</Button>
-      </div>
-      <div className="flex flex-row space-x-8 mt-8">
-        <div className="flex flex-col space-y-4">
-          <Button variant="outline">Sign In</Button>
-          <Button variant="outline">Sign Up</Button>
-        </div>
-        <div className="flex flex-col space-y-4">
-          <Button variant="outline">Features</Button>
-          <Button variant="outline">Pricing</Button>
-          <Button variant="outline">Contact</Button>
-        </div>
-        <div className="flex flex-col space-y-4">
-          <Button variant="outline">About</Button>
-          <Button variant="outline">Blog</Button>
-          <Button variant="outline">FAQ</Button>
-        </div>
-        <div>
-          <Button variant="outline">More</Button>
-        </div>
-      </div>
+    <div className="min-h-screen flex flex-col ">
+      {/* Header */}
+      <Navbar  />
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* Features Section */}
+      <FeatureSection />
+
+      {/* User Reviews Section */}
+      <UserReviews />
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
