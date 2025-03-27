8715b15bb2e66fc4d154184c1eaf6994c3523037
pankaj kumar <pankajamsali@gmail.com>
2025-03-27 23:06:07 +0530

    implement server-side session retrieval in Dashboard; integrate DashNav component and fix ProfileMenu import

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,7 +1,16 @@
+import DashNav from '@/components/dashboard/DashNav';
 import React from 'react';
+import { authOption, CustomSession } from '../api/auth/[...nextauth]/options';
+import { getServerSession } from 'next-auth';
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
-
-export default Dashboard;
+export default async function dashboard() {
+  const session: CustomSession | null = await getServerSession(authOption);
+  return (
+    <div>
+      <DashNav
+        name={session?.user?.name!}
+        image={session?.user?.image ?? undefined}
+      />
+    </div>
+  );
+}
