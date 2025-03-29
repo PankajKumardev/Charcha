fa185133ec2b3c30860169d4d1f5b3dd1f23fc43
pankaj kumar <pankajamsali@gmail.com>
2025-03-29 15:39:45 +0530

    add CreateChat component and validation; implement chat creation functionality in dashboard

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import DashNav from '@/components/dashboard/DashNav';
 import React from 'react';
 import { authOption, CustomSession } from '../api/auth/[...nextauth]/options';
 import { getServerSession } from 'next-auth';
+import CreateChat from '@/components/groupChat/CreateChat';
 
 export default async function dashboard() {
   const session: CustomSession | null = await getServerSession(authOption);
@@ -11,7 +12,20 @@ export default async function dashboard() {
         name={session?.user?.name!}
         image={session?.user?.image ?? undefined}
       />
-      <p>{JSON.stringify(session)}</p>
+
+      <div className="container">
+        <div className="mt-6 text-end">
+          <CreateChat user={session?.user!} />
+        </div>
+
+        {/* If Groups
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {groups.length > 0 &&
+            groups.map((item, index) => (
+              <GroupChatCard group={item} key={index} user={session?.user!} />
+            ))}
+        </div> */}
+      </div>
     </div>
   );
 }
