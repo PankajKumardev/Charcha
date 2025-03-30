0c9903f2a214d21e441e73c718d1793d99e164a4
pankaj kumar <pankajamsali@gmail.com>
2025-03-30 22:01:21 +0530

    enhance NotFound page with user-friendly messaging; comment out unused group chat display in dashboard

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -19,12 +19,12 @@ export default async function dashboard() {
         </div>
 
         {/* If Groups
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {groups.length > 0 &&
-            groups.map((item, index) => (
-              <GroupChatCard group={item} key={index} user={session?.user!} />
-            ))}
-        </div> */}
+        // <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        //   {groups.length > 0 &&
+        //     groups.map((item, index) => (
+        //       <GroupChatCard group={item} key={index} user={session?.user!} />
+        //     ))}
+        // </div> */}
       </div>
     </div>
   );
--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -4,6 +4,15 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className="flex justify-center items-center flex-col h-screen">
+      <h1 className="text-4xl font-bold text-center mb-4">
+        404 - Page Not Found
+      </h1>
+      <p className="text-lg text-center mb-8">
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <p className="text-lg text-center mb-8">
+        You can go back to the homepage by clicking the button below.
+      </p>
       <Link href="/">
         <Button>Return Home</Button>
       </Link>
